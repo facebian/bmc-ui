ef04a1d025dd168dca77a1e9ee52b5669b045276
Siarhei Mlynchyk <sergmlynchyk@gmail.com>
2025-03-21 00:12:40 +0100

    add some settings, components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import "./App.css";
-import { LoginForm } from "./components/login-form";
+import './App.css'
+import { LoginForm } from './components/login-form'
 
 function App() {
   return (
-    <div className='flex min-h-svh w-full items-center justify-center p-6 md:p-10'>
-      <div className='w-full max-w-sm'>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
         <LoginForm />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
